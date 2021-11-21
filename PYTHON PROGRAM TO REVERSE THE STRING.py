511c973628fa1a229125c6bf37ac9cea3713a1f2
AJSPARK31 <88500955+AJSPARK31@users.noreply.github.com>
2021-11-21 22:31:03 +0530

    Add files via upload

---
+++ PYTHON PROGRAM TO REVERSE THE STRING.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+my_string ="1234abcd"
+def reverse_string():
+    reversed_string = my_string[::-1]
+    return(reversed_string)
+print(reverse_string())
+
+
+# In[ ]:
+
+
+
+
